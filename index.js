f0186642e47677c30d2eb68cbc01e144da289dc9
sahil bishnoi <sahilbishnoi170@gmail.com>
2023-11-06 20:37:45 +0530

    fixed bugs of itinerary

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use("/uploads", express.static("uploads"));
 
 app.use(cors());
-app.use(express.json({limit:'100mb'}));
+app.use(express.json({ limit: "100mb" }));
 
 // Available Routes
 app.use("/api/auth", require("./routes/auth"));
@@ -33,15 +33,17 @@ app.post("/api/upload", (req, res) => {
       const filesArray = files.files;
       const imagePaths = [];
 
-      for (const file of filesArray) {
-        const oldPath = file.filepath;
-        const fileName = `${Date.now()}_${file.originalFilename}`;
-        const newPath = path.join(__dirname, "uploads", fileName);
+      if (filesArray) {
+        for (const file of filesArray) {
+          const oldPath = file.filepath;
+          const fileName = `${Date.now()}_${file.originalFilename}`;
+          const newPath = path.join(__dirname, "uploads", fileName);
 
-        await fsPromises.rename(oldPath, newPath);
+          await fsPromises.rename(oldPath, newPath);
 
-        const imagePath = `/uploads/${fileName}`;
-        imagePaths.push(imagePath);
+          const imagePath = `/uploads/${fileName}`;
+          imagePaths.push(imagePath);
+        }
       }
 
       return res.status(200).json({ imagePaths });
@@ -51,6 +53,7 @@ app.post("/api/upload", (req, res) => {
     }
   });
 });
+
 app.post("/api/upload/itinerary", (req, res) => {
   const form = new formidable.IncomingForm();
 
@@ -60,25 +63,20 @@ app.post("/api/upload/itinerary", (req, res) => {
     }
 
     try {
-      const filesArray = files.files;
+      const file = files.pdfFile[0];
 
-      const itineraryPaths = [];
+      const oldPath = file.filepath;
+      const fileName = `${Date.now()}_${file.originalFilename}`;
+      const newPath = path.join(__dirname, "uploads", fileName);
 
-      for (const file of filesArray) {
-        const oldPath = file.filepath;
-        const fileName = `${Date.now()}_${file.originalFilename}`;
-        const newPath = path.join(__dirname, "uploads", fileName);
+      await fsPromises.rename(oldPath, newPath);
 
-        await fsPromises.rename(oldPath, newPath);
+      const itineraryPath = `/uploads/${fileName}`;
 
-        const itineraryPath = `/uploads/${fileName}`;
-        itineraryPaths.push(itineraryPath);
-      }
-
-      return res.status(200).json({ itineraryPaths });
+      return res.status(200).json({ itineraryPath });
     } catch (error) {
-      console.error("Error moving files:", error);
-      return res.status(500).json({ error: "Error moving files" });
+      console.error("Error moving file:", error);
+      return res.status(500).json({ error: "Error moving file" });
     }
   });
 });
